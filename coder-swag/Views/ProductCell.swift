e9a31e80af2e3913a90abd35f186bb485c7fb67b
Billy Morris <Rivers@Billys-MacBook-Pro.local>
2017-08-31 20:42:52 -0700

    set up collection view in storyboard and created collection cell view

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Billy Morris on 8/31/17.
+//  Copyright © 2017 Billy Morris. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+
+
+
+}
